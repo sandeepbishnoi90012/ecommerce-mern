4a2adf58ae866ab0f3ead2a3812d11d48989be1b
sandeep bishnoi <121859276+sandeepbishnoi90012@users.noreply.github.com>
2023-10-23 14:18:40 +0530

    Update server.js
    server.js updated

--- server.js
+++ server.js
@@ -7,12 +7,17 @@ import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import path from "path";
+import {fileURLToPath} from 'url';
+
 // configure env
 dotenv.config();
 
 // connect to DB
 connectToDB();
 
+// esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 // middleware
